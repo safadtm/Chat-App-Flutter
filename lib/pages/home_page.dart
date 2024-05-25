87268a63d6e373f9d1aaf89fc5c867e417025629
safadtm <safadtm2004@gmail.com>
2024-05-25 21:03:59 +0530

    Drawer created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,30 +1,19 @@
 import 'package:chat_app/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
+import '../components/my_drawer.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void logout() {
-    // get auth service
-    final _auth = AuthService();
-    _auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
-        actions: [
-          // logout button
-          IconButton(
-            onPressed: logout,
-            icon: Icon(
-              Icons.logout,
-            ),
-          ),
-        ],
       ),
+      drawer: const MyDrawer(),
     );
   }
 }
