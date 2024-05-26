fcd5302b446349d223a977f78245408188a27919
safadtm <safadtm2004@gmail.com>
2024-05-26 13:00:44 +0530

    overflow bug fix

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -47,76 +47,78 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Logo
-            Icon(
-              Icons.message,
-              size: 60,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            const SizedBox(height: 50),
-            //  welcome back message
-            Text(
-              "Welcome back, you've been missed!",
-              style: TextStyle(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo
+              Icon(
+                Icons.message,
+                size: 60,
                 color: Theme.of(context).colorScheme.primary,
-                fontSize: 16,
               ),
-            ),
-
-            const SizedBox(height: 25),
-
-            // email textfield
-            MyTextField(
-              hintText: "Email",
-              obscureText: false,
-              controller: _emailController,
-            ),
-
-            const SizedBox(height: 10),
-
-            // pw textfield
-            MyTextField(
-              hintText: "Password",
-              obscureText: true,
-              controller: _pwController,
-            ),
-
-            const SizedBox(height: 25),
-
-            // login button
-            MyButton(
-              text: "Login",
-              onTap: () => login(context),
-            ),
-
-            const SizedBox(height: 25),
-
-            // register now
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Not a member? ",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
+              const SizedBox(height: 50),
+              //  welcome back message
+              Text(
+                "Welcome back, you've been missed!",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16,
                 ),
-                GestureDetector(
-                  onTap: onTap,
-                  child: Text(
-                    "Register now",
+              ),
+          
+              const SizedBox(height: 25),
+          
+              // email textfield
+              MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: _emailController,
+              ),
+          
+              const SizedBox(height: 10),
+          
+              // pw textfield
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: _pwController,
+              ),
+          
+              const SizedBox(height: 25),
+          
+              // login button
+              MyButton(
+                text: "Login",
+                onTap: () => login(context),
+              ),
+          
+              const SizedBox(height: 25),
+          
+              // register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Not a member? ",
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
                       color: Theme.of(context).colorScheme.primary,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      "Register now",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -59,88 +59,90 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(   
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Logo
-            Icon(
-              Icons.message,
-              size: 60,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            const SizedBox(height: 50),
-            //  welcome back message
-            Text(
-              "Let's create an account for you",
-              style: TextStyle(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo
+              Icon(
+                Icons.message,
+                size: 60,
                 color: Theme.of(context).colorScheme.primary,
-                fontSize: 16,
               ),
-            ),
-
-            const SizedBox(height: 25),
-
-            // email textfield
-            MyTextField(
-              hintText: "Email",
-              obscureText: false,
-              controller: _emailController,
-            ),
-
-            const SizedBox(height: 10),
-
-            // pw textfield
-            MyTextField(
-              hintText: "Password",
-              obscureText: true,
-              controller: _pwController,
-            ),
-
-            const SizedBox(height: 10),
-
-            // confirm pw textfield
-            MyTextField(
-              hintText: "Confirm password",
-              obscureText: true,
-              controller: _confirmPwController,
-            ),
-
-            const SizedBox(height: 25),
-
-            // login button
-            MyButton(
-              text: "Register",
-              onTap: () => register(context),
-            ),
-
-            const SizedBox(height: 25),
-
-            // register now
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account? ",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
+              const SizedBox(height: 50),
+              //  welcome back message
+              Text(
+                "Let's create an account for you",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16,
                 ),
-                GestureDetector(
-                  onTap: onTap,
-                  child: Text(
-                    "Login now",
+              ),
+          
+              const SizedBox(height: 25),
+          
+              // email textfield
+              MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: _emailController,
+              ),
+          
+              const SizedBox(height: 10),
+          
+              // pw textfield
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: _pwController,
+              ),
+          
+              const SizedBox(height: 10),
+          
+              // confirm pw textfield
+              MyTextField(
+                hintText: "Confirm password",
+                obscureText: true,
+                controller: _confirmPwController,
+              ),
+          
+              const SizedBox(height: 25),
+          
+              // login button
+              MyButton(
+                text: "Register",
+                onTap: () => register(context),
+              ),
+          
+              const SizedBox(height: 25),
+          
+              // register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account? ",
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
                       color: Theme.of(context).colorScheme.primary,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      "Login now",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
