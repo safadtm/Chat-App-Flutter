471c4f256276580f82d14f2721b4e119e9dd26a2
safadtm <safadtm2004@gmail.com>
2024-05-26 10:51:13 +0530

    Dark Mode

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatBubble extends StatelessWidget {
   final String message;
@@ -11,16 +13,25 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // light vs dark mode for correct bubble colors
+    bool isDarkMode =
+        Provider.of<ThemeProvider>(context, listen: false).isDarkMode;
+
     return Container(
       decoration: BoxDecoration(
-        color: isCurrentUser ? Colors.green : Colors.grey.shade500,
-      borderRadius: BorderRadius.circular(12)
-      ),
+          color: isCurrentUser
+              ? (isDarkMode ? Colors.green.shade600 : Colors.grey.shade500)
+              : (isDarkMode ? Colors.grey.shade800 : Colors.grey.shade200),
+          borderRadius: BorderRadius.circular(12)),
       padding: const EdgeInsets.all(16),
       margin: EdgeInsets.symmetric(vertical: 2.5, horizontal: 25),
       child: Text(
         message,
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(
+          color: isCurrentUser
+              ? Colors.white
+              : (isDarkMode ? Colors.white : Colors.black),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:chat_app/services/auth/auth_gate.dart';
 import 'package:chat_app/firebase_options.dart';
-import 'package:chat_app/themes/light_mode.dart';
+import 'package:chat_app/themes/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'services/auth/login_or_register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,8 +26,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const AuthGate(),
-      theme: lightMode,
+      home: const AuthGate(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
